86cfad30fdfe88c56d86e22df840d5548f1aaa12
adrie <adrie@192.168.1.58>
2022-01-25 15:16:32 +0100

    first commit project jee

---
+++ web-app1/src/main/java/fr/formation/inti/controller/RedirectServlet.java
@@ -0,0 +1,46 @@
+package fr.formation.inti.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RedirectServlet
+ */
+@WebServlet("/redirect")
+public class RedirectServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RedirectServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Set response content type
+	    response.setContentType("text/html");
+
+	    // New location to be redirected
+	    String site = new String("http://localhost:8080/web-app1/infos");
+
+	    response.setStatus(response.SC_MOVED_TEMPORARILY);
+	    response.setHeader("Location", site);    
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
