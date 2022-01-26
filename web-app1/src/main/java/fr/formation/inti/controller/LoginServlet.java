24f8a0cd847dfc4fc3057167b5556084366657d3
adrie <adrie@192.168.1.58>
2022-01-26 08:25:31 +0100

    mise à jour SessionServlet

--- web-app1/src/main/java/fr/formation/inti/controller/LoginServlet.java
+++ web-app1/src/main/java/fr/formation/inti/controller/LoginServlet.java
@@ -2,18 +2,22 @@ package fr.formation.inti.controller;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-@WebServlet("/login")
+
 public class LoginServlet extends HttpServlet {
 
 	private final static Log log = LogFactory.getLog(LoginServlet.class);
@@ -32,18 +36,7 @@ public class LoginServlet extends HttpServlet {
 	 *      response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String login = request.getParameter("login");
-		String password = request.getParameter("password");
-
-		 if ("root".equals(login) && "123456".equals(password)) {
-			Writer out = response.getWriter();
-			out.append("<html>").append("<head><title>login success</title></head>").append("<body>")
-					.append("<h1>Hello " + request.getParameter("login") + "</h1>").append("<p>You are connected!</p>")
-					.append("<p>Date : " + new Date() + "</p>");
-
-		} else {
-			response.sendRedirect(request.getContextPath() + "/index.html");
-		}
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
@@ -52,7 +45,22 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		doGet(request, response);
+		String login = request.getParameter("Login");
+		
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        String date = dtf.format(LocalDateTime.now()).toString();
+
+		ServletOutputStream out = response.getOutputStream();
+		out.println("<html>");
+		out.println("<head><title>Login</title></head>");
+		out.println("<body>");
+		out.println("<p>hello "+login+"</p>");
+		out.println("<p>you are connected</p>");
+		out.println("<p>"+date+"</p>");
+		
+		out.println("<form action=\"index.html\" method=\"post\">"		
+				+ "<input type=\"submit\" value=\"Retour home\">"		
+				+ "</form>");
 	}
 
 }
